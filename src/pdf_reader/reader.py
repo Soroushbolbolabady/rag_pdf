b8763395561c126b852c6fee9bac2f09d2f92d95
Soroush Bolbolabady <Soroushbolbolabady@gmail.com>
2026-02-24 10:29:02 +0100

    feature: create Tokenize funtion for inputs. create upload route to upload the pdf files

--- src/pdf_reader/reader.py
+++ src/pdf_reader/reader.py
@@ -6,9 +6,10 @@ import nltk
 nltk.download('punkt_tab')
 
 
+pdf_path = "src/pdf_reader/01.pdf"
+pdf2_path = "src/pdf_reader/02.pdf"
 
-reader = PdfReader("src/pdf_reader/01.pdf")
-pages_list = []
+
 
 
 
@@ -20,18 +21,24 @@ def text_refactor(txt: str) -> str:
 
 
 
-for i in range(len(reader.pages)):
-    
+def tokenize_pdf(pdf_path : str) -> list[dict]:
 
-    page_dict = {}
-    page = reader.pages[i]
-    page_dict["page_number"] = page.page_number
-    text = page.extract_text()
-    text = text_refactor(text)
-    page_dict["page_text"] = text
-    sentences = sent_tokenize(text)
-    page_dict["page_text_tokenized"] = sentences
-    page_dict["token_count"] = len(text) / 4
-    pages_list.append(page_dict)
+    reader = PdfReader(pdf_path)
     
+    pages_list = []
+    
+    for i in range(len(reader.pages)):
+        page_dict = {}
+        page = reader.pages[i]
+        page_dict["page_number"] = page.page_number
+        text = page.extract_text()
+        text = text_refactor(text)
+        page_dict["page_text"] = text
+        sentences = sent_tokenize(text)
+        page_dict["page_text_tokenized"] = sentences
+        page_dict["token_count"] = len(text) / 4
+        pages_list.append(page_dict)
+    
+    return pages_list
+
 
