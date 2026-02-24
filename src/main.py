e383f86ab20a569f1efaf2a61d79001582c2247b
Soroush Bolbolabady <Soroushbolbolabady@gmail.com>
2026-02-24 10:29:49 +0100

    feature: add pdf route to main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from src.routers import pdf
 from src.db.create_db import create_db_and_tables
 
 
@@ -8,3 +8,6 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+app.include_router(pdf.router)
